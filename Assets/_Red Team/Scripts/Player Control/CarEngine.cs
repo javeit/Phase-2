3ea65c14cd75b48b65a923a7680200db9ee5a867
javeit1995 <javeit@ostatemail.okstate.edu>
2018-04-26 10:17:13 -0500

    Test CarEngine script

---
+++ Assets/_Red Team/Scripts/Player Control/CarEngine.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarEngine : MonoBehaviour {
+
+	/// <summary>
+	/// The max acceleration.
+	/// </summary>
+	public float maxAcceleration;
+
+	/// <summary>
+	/// The max speed.
+	/// </summary>
+	public float maxSpeed;
+
+	/// <summary>
+	/// The speed of the car when it is on and the player is not doing anything
+	/// </summary>
+	public float idleSpeed;
+
+	/// <summary>
+	/// The rate at which the car accelerates/decelerates towards it's idle speed
+	/// </summary>
+	public float idleAcelleration;
+
+	/// <summary>
+	/// The max deceleration.
+	/// </summary>
+	public float maxDeceleration;
+
+	/// <summary>
+	/// The max turning angle.
+	/// </summary>
+	public float maxTurnAngle;
+
+	/// <summary>
+	/// The max stoping speed.
+	/// </summary>
+	public float maxStopingSpeed;
+
+	public bool isOn;
+
+	/// <summary>
+	/// Gets or sets the turn angle while ensuring that it doesn't exceed the max value
+	/// </summary>
+	/// <value>The turn angle.</value>
+	public float TurnAngle {
+		get {
+			return turnAngle;
+		}
+		set {
+			turnAngle = Mathf.Clamp (value, -1 * maxTurnAngle, maxTurnAngle);
+		}
+	}
+
+	public float speed;
+	public float turnAngle;
+
+	public void Accelerate(float acceleration) {
+		// ensure acceleration does not exceed the max or result in a deccelleration
+		acceleration = Mathf.Clamp (acceleration, 0, maxAcceleration);
+
+		if (!isOn)
+			return;
+
+		speed += acceleration * Time.deltaTime;
+		speed = Mathf.Clamp (speed, 0, maxSpeed);
+	}
+
+	public void Decelerate(float deceleration) {
+		// ensure acceleration does not exceed the max or result in an acceleration
+		deceleration = Mathf.Clamp (deceleration, 0, maxDeceleration);
+
+		if (!isOn)
+			return;
+
+		speed -= deceleration * Time.deltaTime;
+		speed = Mathf.Clamp (speed, 0, maxSpeed);
+	}
+
+	public void TurnOff() {
+		if (isOn && speed <= maxStopingSpeed)
+			isOn = false;
+		else
+			Debug.Log ("Cannot stop at this speed; Need to slow down.");
+	}
+
+	public void TurnOn() {
+		isOn = true;
+	}
+
+	void Update() {
+
+		transform.Rotate (transform.up, turnAngle * Time.deltaTime);
+		transform.Translate (transform.forward * speed * Time.deltaTime);
+
+//		if (isOn) {
+//			if (speed != idleSpeed) {
+//				if (Mathf.Abs (speed - idleSpeed) < idleAcelleration * Time.deltaTime) {
+//					speed = idleSpeed;
+//				} else if (speed > idleSpeed) {
+//					Decelerate (idleAcelleration);
+//				} else {
+//					Accelerate (idleAcelleration);
+//				}
+//			}
+//		} else 
+		if(!isOn){
+			if(speed != 0)
+				Decelerate (maxDeceleration);
+
+			if (turnAngle != 0)
+				turnAngle = 0;
+		}
+	}
+
+	void Start() {
+		speed = 0f;
+		turnAngle = 0f;
+	}
+}
---
+++ Assets/_Red Team/Scripts/Player Control/SimpleCarController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CarEngine))]
+public class SimpleCarController : MonoBehaviour {
+
+	CarEngine engine;
+
+	void Update () {
+		float acceleration = Input.GetAxis ("Vertical");
+		float turnAngle = Input.GetAxis ("Horizontal");
+
+		//Debug.Log ("Acceleration Input: " + acceleration);
+
+		if (acceleration > 0)
+			engine.Accelerate (acceleration * engine.maxAcceleration);
+		else if (acceleration < 0)
+			engine.Decelerate (Mathf.Abs(acceleration) * engine.maxDeceleration);
+
+		engine.TurnAngle = turnAngle * engine.maxTurnAngle;
+	}
+
+	void Start() {
+		engine = GetComponent<CarEngine> ();
+	}
+}
