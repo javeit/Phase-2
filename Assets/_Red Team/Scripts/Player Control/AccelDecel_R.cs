f5199a27f047b8d8955f6f678cc88d65b1b5e849
DESKTOP-4C0RU67\ImmersiveComputing <carson.hogstad@gmail.com>
2018-05-04 15:28:22 -0600

    testing VR

--- Assets/_Red Team/Scripts/Player Control/AccelDecel_R.cs
+++ Assets/_Red Team/Scripts/Player Control/AccelDecel_R.cs
@@ -1,21 +1,16 @@
-﻿namespace VRTK.Examples
+﻿namespace RedTeam
 {
-    using UnityEngine;
+	using UnityEngine;
 
+	[RequireComponent(typeof(SteamVR_TrackedObject))]
     public class AccelDecel_R : MonoBehaviour
     {
         private void Start()
-        {
+		{
 
-            if (GetComponent<VRTK_ControllerEvents>() == null)
-            {
-                VRTK_Logger.Error(VRTK_Logger.GetCommonMessage(VRTK_Logger.CommonMessageKeys.REQUIRED_COMPONENT_MISSING_FROM_GAMEOBJECT, "VRTK_ControllerEvents_ListenerExample", "VRTK_ControllerEvents", "the same"));
-                return;
-            }
-
-            //Setup controller event listeners
-            GetComponent<VRTK_ControllerEvents>().TriggerPressed += new ControllerInteractionEventHandler(DoTriggerPressed);
-            GetComponent<VRTK_ControllerEvents>().TriggerReleased += new ControllerInteractionEventHandler(DoTriggerReleased);
+//            //Setup controller event listeners
+//			GetComponent<SteamVR_TrackedObject>(). .TriggerPressed += new ControllerInteractionEventHandler(DoTriggerPressed);
+//			GetComponent<SteamVR_TrackedObject>().TriggerReleased += new ControllerInteractionEventHandler(DoTriggerReleased);
 
         }
 
@@ -24,19 +19,19 @@
         public float decel = 5;
         public float speed = 0;
 
-        private void DoTriggerPressed(object sender, ControllerInteractionEventArgs e)
-        {
-            speed += accel * Time.fixedDeltaTime;
-            Debug.Log("L pressed");
-
-        }
-
-        private void DoTriggerReleased(object sender, ControllerInteractionEventArgs e)
-        {
-            speed -= accel * Time.fixedDeltaTime;
-            Debug.Log("L released");
-
-        }
+//        private void DoTriggerPressed(object sender, ControllerInteractionEventArgs e)
+//        {
+//            speed += accel * Time.fixedDeltaTime;
+//            Debug.Log("L pressed");
+//
+//        }
+//
+//        private void DoTriggerReleased(object sender, ControllerInteractionEventArgs e)
+//        {
+//            speed -= accel * Time.fixedDeltaTime;
+//            Debug.Log("L released");
+//
+//        }
 
         void FixedUpdate()
         {
